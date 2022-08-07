15faa46d358d7b57a5f3099937338ef163a24f37
Leonardo <leonardoorosaa16@gmail.com>
2022-08-07 19:27:42 -0300

    work(android): added android plugin

---
+++ packages/flutter_onfido_plus_android/lib/flutter_onfido_plus_android.dart
@@ -0,0 +1,2 @@
+
+export 'flutter_onfido_plus_android_method_channel.dart';
---
+++ packages/flutter_onfido_plus_android/lib/flutter_onfido_plus_android_method_channel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_onfido_plus_platform_interfaces/flutter_onfido_plus_platform_interfaces.dart';
+
+const MethodChannel _channel =
+    MethodChannel('plugins.flutter.io/flutter_onfido_plus_android');
+
+/// An implementation of [FlutterOnfidoPlusAndroidPlatform] that uses method channels.
+class FlutterOnfidoPlusAndroid extends FlutterOnfidoPlusPlatform {
+
+  static void registerWith() {
+    FlutterOnfidoPlusPlatform.instance = FlutterOnfidoPlusAndroid();
+  }
+
+  @override
+  Future initialize(FlutterOnfidoSettings settings) {
+    return _channel.invokeMethod('initialize', settings.toMap(),);
+  }
+}
